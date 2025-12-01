9705997bfb683bef4aca3848a75c9d73a86d14d5
Hamres <h4mres@gmail.com>
2025-12-01 15:12:39 +0300

    add fetching articles, view type selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+import { ArticleViewSelector } from './ArticleViewSelector';
+
+const meta = {
+  title: 'shared/ArticleViewSelector',
+  component: ArticleViewSelector,
+} satisfies Meta<typeof ArticleViewSelector>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,51 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import SmallIcon from 'shared/assets/icons/tripalki.svg';
+import BigIcon from 'shared/assets/icons/kvadrat.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { ArticleView } from '../../model/types/article';
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+  className?: string;
+  view: ArticleView,
+  onViewClick?: (view: ArticleView) => void
+}
+
+const viewTypes = [
+  {
+    view: ArticleView.SMALL,
+    icon: BigIcon,
+  },
+  {
+    view: ArticleView.BIG,
+    icon: SmallIcon,
+  },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+  const { t } = useTranslation();
+  const { className, onViewClick, view } = props;
+
+  const onClick = (newView: ArticleView) => () => {
+    onViewClick?.(newView);
+  };
+
+  return (
+    <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+      {viewTypes.map(((viewType) => (
+        <Button
+          theme={ButtonTheme.CLEAR}
+          onClick={onClick(viewType.view)}
+        >
+          <Icon
+            Svg={viewType.icon}
+            className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+          />
+        </Button>
+      )))}
+    </div>
+  );
+});
