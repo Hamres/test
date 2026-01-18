62caaf74e927a8299376c949f1a4a1563170eed7
Hamres <h4mres@gmail.com>
2026-01-18 18:09:05 +0300

    update custom eslint plugin, add support for alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,7 +71,7 @@ module.exports = {
     'no-undef': 'off',
     'react/no-array-index-key': 'off',
     'arrow-body-style': 'off',
-    'test-eslint-plugin-v1/path-checker': 'error',
+    'test-eslint-plugin-v1/path-checker': ['error', { alias: '@' }],
   },
   globals: {
     __IS_DEV__: true,
