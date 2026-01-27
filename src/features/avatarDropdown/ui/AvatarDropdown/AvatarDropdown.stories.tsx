5c774240af0c71e602ed2acb4db81db1274af7ad
Hamres <h4mres@gmail.com>
2026-01-27 14:59:05 +0300

    add story AvatarDropdown.stories.tsx, fix husky

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import { AvatarDropdown } from './AvatarDropdown';
+import { StoreDecorator } from '@/shared/config/storybook';
 
 const meta = {
   title: 'features/AvatarDropdown',
@@ -12,4 +13,5 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {},
+  decorators: [StoreDecorator({ user: { authData: { id: '1', roles: 'ADMIN' }, _inited: true } })],
 };
