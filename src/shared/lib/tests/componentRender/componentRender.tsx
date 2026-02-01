a83fb9a09749452b5b6d54f7c77f3accb2d3ec8d
Hamres <h4mres@gmail.com>
2026-02-01 12:23:14 +0300

    add tests for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,28 +5,48 @@ import { MemoryRouter } from 'react-router-dom';
 import { ReducersMapObject } from '@reduxjs/toolkit';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line test-eslint-plugin-v1/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line test-eslint-plugin-v1/layer-imports
+import '@/app/styles/index.scss';
 
 export interface componentRouterOptions {
   route?: string
   initialState?: DeepPartial<StateSchema>
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
-
+  theme?: Theme
 }
 
-export function componentRender(component: ReactNode, options: componentRouterOptions = {}) {
+interface TestProviderProps {
+  children?: ReactNode
+  options?: componentRouterOptions
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+
   const {
     route = '/',
     initialState,
     asyncReducers,
+    theme = Theme.LIGHT,
   } = options;
 
-  return render(
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
         <I18nextProvider i18n={i18nForTests}>
-          {component}
+          <ThemeProvider>
+            {children}
+
+          </ThemeProvider>
         </I18nextProvider>
       </StoreProvider>
-    </MemoryRouter>,
+    </MemoryRouter>
   );
+}
+
+export function componentRender(component: ReactNode, options: componentRouterOptions = {}) {
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
