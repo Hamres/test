13a5107bad9e58556a930e02723e24a0b2e2cca6
Hamres <h4mres@gmail.com>
2026-02-01 11:21:44 +0300

    skip one test, add stubs(fixtures)

--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -21,4 +21,16 @@ describe('Пользователь заходит на страницу со с�
     cy.wait(1000);
     cy.getByTestId('ArticleListItem').should('have.length', 1);
   });
+
+  it('На стабах ()', () => {
+    cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+  });
+
+  it.skip('скипается', () => {
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+    cy.getByTestId('awdawdawd').should('exist');
+  });
 });
