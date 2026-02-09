b989966dbdb39a5f3b96174ef9a092b4830b728e
Hamres <h4mres@gmail.com>
2026-02-09 12:06:05 +0300

    add toggle flags and auto remove scripts

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,5 +1,6 @@
 import { counterReducer } from './model/slice/CounterSlice';
 import type { CounterSchema } from './model/types/CounterSchema';
 import { Counter } from './ui/Counter';
+import { CounterRedesigned } from './ui/CounterRedesigned';
 
-export { counterReducer, Counter, CounterSchema };
+export { counterReducer, Counter, CounterSchema, CounterRedesigned };
---
+++ src/entities/Counter/ui/CounterRedesigned.tsx
@@ -0,0 +1,37 @@
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { Button } from '@/shared/ui/Button';
+import { useCounterActions } from '../model/slice/CounterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+
+export const CounterRedesigned = () => {
+  const dispatch = useDispatch();
+  const counterValue = useCounterValue();
+  const { t } = useTranslation();
+  const { add, decrement, increment } = useCounterActions();
+
+  const handleInc = () => {
+    increment();
+  };
+
+  const handleDec = () => {
+    decrement();
+  };
+
+  const handleAddFive = () => {
+    add(10);
+  };
+
+  return (
+    <div>
+      <h1 data-testid="value-title">{counterValue}</h1>
+      <Button onClick={handleInc} data-testid="increment-btn">
+        {t('increment')}
+      </Button>
+      <Button onClick={handleDec} data-testid="decrement-btn">
+        {t('decrement')}
+      </Button>
+      <Button onClick={handleAddFive}>{t('addTen')}</Button>
+    </div>
+  );
+};
